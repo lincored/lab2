614205a20f590672f88206b845395c63ee363164
ser <leg@mail.com>
2025-12-06 15:35:14 +0700

    fix

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
-#!/usr/bin/env python3
 import http.server
 import socketserver
 import os
 
 PORT = 8080
 
-# Переходим в рабочую директорию
 os.chdir('/app')
 
 Handler = http.server.SimpleHTTPRequestHandler
-
-print(f"Starting web server on port {PORT}")
-print(f"Server URL: http://localhost:{PORT}")
-print("Press Ctrl+C to stop the server")
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     try:
